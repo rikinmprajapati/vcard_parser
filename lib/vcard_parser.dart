32b09fde6be0837b968897acad61be71820e777c
Rikin <rikin.prajapati@known2me.com>
2021-12-10 17:26:12 +0530

    null safety has been added

--- lib/vcard_parser.dart
+++ lib/vcard_parser.dart
@@ -1,13 +1,17 @@
 library vcard_parser;
 
 class VcardParser {
-  final VCARD_BEGIN_SIGN      = 'BEGIN:VCARD';
-  final VCARD_END_SIGN        = 'END:VCARD';
-  final VCARD_FIELD_SEPARATORS = [';', '='];
-  final VCARD_TAG_SEPARATOR = '\n';
-  final VCARD_TAG_KE_VALUE_SEPARATOR = ':';
-  final List<String> VCARD_TAG_VALUE_IGONE_SEPARATOR = [',', ' '];
-  final VCARD_TAGS = [
+  static const VCARD_BEGIN_SIGN = 'BEGIN: VCARD';
+
+  static const VCARD_END_SIGN = 'END: VCARD';
+
+  static const VCARD_FIELD_SEPARATORS = [';', '='];
+  static const VCARD_TAG_SEPARATOR = ' \ n ';
+
+  static const VCARD_TAG_KE_VALUE_SEPARATOR = ':';
+
+  static const List<String> VCARD_TAG_VALUE_IGONE_SEPARATOR = [',', ' '];
+  static const VCARD_TAGS = [
     'ADR',
     'AGENT',
     'BDAY',
@@ -32,7 +36,7 @@ class VcardParser {
     'REV',
     'ROLE',
     'SORT-STRING'
-    'SOUND',
+        'SOUND',
     'SOURCE',
     'TEL',
     'TITLE',
@@ -42,53 +46,54 @@ class VcardParser {
     'VERSION',
   ];
 
-  String content = null;
-  Map<String, Object> tags = new Map<String,Object>();
+  String? content;
+  Map<String, Object> tags = new Map<String, Object>();
+
   VcardParser(String content) {
-    this.content= content;
+    this.content = content;
     print(content);
   }
 
   Map<String, Object> parse() {
-    Map<String,Object> tags = new Map<String, Object>();
+    Map<String, Object> tags = new Map<String, Object>();
 
-    // 去除;; 并将文本内容按行切分
-    List<String> lines = content.replaceAll(";;", "").split(VCARD_TAG_SEPARATOR);
+    // Remove;; and split the text content into lines
+    List<String> lines = (content ?? '').replaceAll(";;", "").split(VCARD_TAG_SEPARATOR);
     lines.forEach((field) {
-      String key = null;
-      Object value= null;
+      String? key;
+      Object value;
 
-      // 提取每行中的标签和值
+      // Extract the label and value in each row
       List<String> tagAndValue = field.split(VCARD_TAG_KE_VALUE_SEPARATOR);
       if (tagAndValue.length != 2) {
-        return ;
+        return;
       }
 
       key = tagAndValue[0].trim();
       value = tagAndValue[1].trim().replaceAll(VCARD_TAG_VALUE_IGONE_SEPARATOR[0], VCARD_TAG_VALUE_IGONE_SEPARATOR[1]);
 
-      // 若复杂字段，则需进一步解析数据
+      // If the field is complex, the data needs to be further parsed
       if (key.contains(VCARD_FIELD_SEPARATORS[0])) {
         value = parseFields(field.trim());
       }
 
-      // 新增或合并数据
-      if(tags.containsKey(key)) {
-        List<Map<String,String>> oldValues = new List<Map<String,String>>();
-        // 如果之前未合并，则用List保存旧的和新的数据
-        if(tags[key] is Map) {
-          Map<String,String> oldValue = tags[key];
+      // Add or merge data
+      if (tags.containsKey(key)) {
+        List<Map<String, String>> oldValues = [];
+        // If it has not been merged before, use List to save the old and new data
+        if (tags[key] is Map) {
+          Map<String, String> oldValue = tags[key] as Map<String, String>? ?? {};
           oldValues.add(oldValue);
-          oldValues.add(value);
+          oldValues.add(value as Map<String, String>? ?? {});
           value = oldValues;
         } else {
-          // 之前合并过，则将新数据追加到旧数据
-          oldValues = tags[key];
-          oldValues.add(value);
+          // It has been merged before, then the new data will be appended to the old data
+          oldValues = tags[key] as List<Map<String, String>>? ?? [];
+          oldValues.add(value as Map<String, String>? ?? {});
           value = oldValues;
         }
       }
-      // 将解析后的字段添加到tags
+      // Add the parsed field to tags
       tags[key] = value;
     });
 
@@ -97,32 +102,35 @@ class VcardParser {
   }
 
   /*
-    解析字段
+    Parsing field
    */
   Object parseFields(String line) {
     Object field = new Object();
     List<String> rawFields = line.split(VCARD_FIELD_SEPARATORS[0]);
 
-    // 切分得到的原始字段中，第0个元素便是key，因此忽略
+    // In the original field obtained by segmentation, the 0th element is the key, so it is ignored
     rawFields.getRange(1, rawFields.length).forEach((rawField) {
-        List<String> items = new List<String>();
-        List<String> rawItems = rawField.split(VCARD_FIELD_SEPARATORS[0]);
-        if(rawItems.length == 1) {
-          rawItems = rawField.split(VCARD_FIELD_SEPARATORS[1]);
-        }
+      List<String> items = [];
+      List<String> rawItems = rawField.split(VCARD_FIELD_SEPARATORS[0]);
+      if (rawItems.length == 1) {
+        rawItems = rawField.split(VCARD_FIELD_SEPARATORS[1]);
+      }
 
-        // 提取item的key和value
-        if(rawItems.length > 0) {
-          rawItems.forEach((itemValue) {
-            items = itemValue.split(VCARD_TAG_KE_VALUE_SEPARATOR);
-          });
-        }
+      // Extract the key and value of the item
+      if (rawItems.length > 0) {
+        rawItems.forEach((itemValue) {
+          items = itemValue.split(VCARD_TAG_KE_VALUE_SEPARATOR);
+        });
+      }
 
-        // item的key和value保存在数组，因此其元素数量为2
-        if (items.length == 2) {
-          field = {'name': items.elementAt(0), 'value': items.elementAt(1).replaceAll(VCARD_TAG_VALUE_IGONE_SEPARATOR[0], VCARD_TAG_VALUE_IGONE_SEPARATOR[1])};
-        }
-      });
-      return field;
+      // The key and value of the item are stored in an array, so the number of elements is 2
+      if (items.length == 2) {
+        field = {
+          'name': items.elementAt(0),
+          'value': items.elementAt(1).replaceAll(VCARD_TAG_VALUE_IGONE_SEPARATOR[0], VCARD_TAG_VALUE_IGONE_SEPARATOR[1])
+        };
+      }
+    });
+    return field;
   }
 }
